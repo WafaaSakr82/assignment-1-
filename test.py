e7d391256135b3bd36f39a7bccc34472265e12cd
WafaaSakr82 <la7nelwafaa@gmail.com>
2025-10-05 16:51:42 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+num1 = 25
+num2 = 20 
+num3 = 10
+num4 = 100
+print("item 1 price: "+  str(num1))
+print("item 2 price: " + str(num2))
+print("item 3 price:" + str(num3))
+print("total budget is " + str(num4))
+
+total_cost = num1 + num2 + num3
+print("total cost of items is  "  + str(total_cost))
+
+result = (num4 > total_cost)
+print("is budget enough? "+ str(result))
+
+money_left = num4 - total_cost
+print("money left = " + str(money_left))
+
+
+
+
+
+# total_cost = "item1_price" + "item2_price" + "item3_price" 
+# print(total_cost)
+
+
+
+
+
+
+# print("file one")
+# print(50)
